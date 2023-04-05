03b9a924d96c936ee13c6bcf3588899b0f0b4920
Frederic Henrichs <F.Henrichs@web.de>
2023-04-05 19:07:24 +0200

    Fix Error when connection gets closed by peer.

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fhenrich <fhenrich@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 12:33:07 by fhenrich          #+#    #+#             */
-/*   Updated: 2023/04/05 18:41:13 by fhenrich         ###   ########.fr       */
+/*   Updated: 2023/04/05 19:04:47 by fhenrich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,19 +38,21 @@ int main() {
 	while (true) {
 		int nev = event_queue.wait();
 		for (int i = 0; i < nev; i++) {
-			if (event_queue.getEventList()[i].flags & EV_ERROR) {
+			const struct kevent &event = event_queue.getEventList()[i];
+			if (event.flags & EV_ERROR) {
 				perror("Error in event_list");
 				exit(EXIT_FAILURE);
 			}
-			const struct kevent &event = event_queue.getEventList()[i];
 			if (servers.find(event.ident) == servers.end()) {
-				char buff[256];
-				memset(buff, 0, 256);
-				int ret = recv(event.ident, buff, 256, 0);
-				if (ret == -1)
-					perror("recv()");
-				else
-					std::cout << buff << std::endl;
+				if (!(event.flags & EV_EOF)) {
+					char buff[256];
+					memset(buff, 0, 256);
+					int ret = recv(event.ident, buff, 256, 0);
+					if (ret == -1)
+						perror("recv()");
+					else
+						std::cout << buff << std::endl;
+				}
 				close(event.ident);
 				event_queue.remove(event.ident);
 			} else {
